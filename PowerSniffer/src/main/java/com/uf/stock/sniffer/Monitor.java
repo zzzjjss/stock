a45b8d945ab2860e8d7e148962b3f84256d1e0c7
adm_ZhangJa <adm_ZhangJa@windows10.microdone.cn>
2016-12-07 18:45:35 +0800

    add open market time  control

--- PowerSniffer/src/main/java/com/uf/stock/sniffer/Monitor.java
+++ PowerSniffer/src/main/java/com/uf/stock/sniffer/Monitor.java
@@ -1,6 +1,7 @@
 package com.uf.stock.sniffer;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import com.uf.stock.service.DataSyncService;
 import com.uf.stock.service.StockAnalysisService;
 import com.uf.stock.sniffer.alarm.Alarm;
 import com.uf.stock.sniffer.alarm.bean.StockUpDownPowerMsg;
+import com.uf.stock.util.StockUtil;
 
 public class Monitor extends Thread {
   private boolean isStarted = false;
@@ -30,34 +32,38 @@ public class Monitor extends Thread {
   public void run() {
     while (isStarted) {
       try {
-        List<StockInfo> stocks = dataSyncService.findStocksInMonitor();
-        System.out.println("monitor stocks size:" + stocks.size());
-        if (stocks != null && stocks.size() > 0) {
-          List<UpDownPower> powers = dataSyncService.calculateStocksCurrentPower(stocks);
-          List<StockUpDownPowerMsg> msgs = new ArrayList<StockUpDownPowerMsg>();
-          for (UpDownPower power : powers) {
-            if (power.isUpPower() && power.getUpdownPowerValue() > 1 && power.getTradeInfo().getUpDownRate() > 1) {
-              StockUpDownPowerMsg msg = new StockUpDownPowerMsg();
-              msg.setIsUpPower(true);
-              msg.setPower(power.getUpdownPowerValue());
-              msg.setStockName(power.getStockName());
-              msg.setStockSymbol(power.getTradeInfo().getStockSymbol());
-              msgs.add(msg);
+        if (StockUtil.isOpenTime(new Date())) {
+          List<StockInfo> stocks = dataSyncService.findStocksInMonitor();
+          System.out.println("monitor stocks size:" + stocks.size());
+          if (stocks != null && stocks.size() > 0) {
+            List<UpDownPower> powers = dataSyncService.calculateStocksCurrentPower(stocks);
+            List<StockUpDownPowerMsg> msgs = new ArrayList<StockUpDownPowerMsg>();
+            for (UpDownPower power : powers) {
+              if (power.isUpPower() && power.getUpdownPowerValue() > 1 && power.getTradeInfo().getUpDownRate() > 1) {
+                StockUpDownPowerMsg msg = new StockUpDownPowerMsg();
+                msg.setIsUpPower(true);
+                msg.setPower(power.getUpdownPowerValue());
+                msg.setStockName(power.getStockName());
+                msg.setStockSymbol(power.getTradeInfo().getStockSymbol());
+                msgs.add(msg);
+              }
+              if (!power.isUpPower() && power.getUpdownPowerValue() > 1 && power.getTradeInfo().getUpDownRate() < -1) {
+                StockUpDownPowerMsg msg = new StockUpDownPowerMsg();
+                msg.setIsUpPower(false);
+                msg.setPower(power.getUpdownPowerValue());
+                msg.setStockName(power.getStockName());
+                msg.setStockSymbol(power.getTradeInfo().getStockSymbol());
+                msgs.add(msg);
+              }
             }
-            if (!power.isUpPower() && power.getUpdownPowerValue() > 1 && power.getTradeInfo().getUpDownRate() < -1) {
-              StockUpDownPowerMsg msg = new StockUpDownPowerMsg();
-              msg.setIsUpPower(false);
-              msg.setPower(power.getUpdownPowerValue());
-              msg.setStockName(power.getStockName());
-              msg.setStockSymbol(power.getTradeInfo().getStockSymbol());
-              msgs.add(msg);
+            for (Alarm alarm : alarms) {
+              alarm.alarm(msgs);
             }
           }
-          for (Alarm alarm : alarms) {
-            alarm.alarm(msgs);
-          }
+          Thread.sleep(1000 * 5);
+        }else{
+          Thread.sleep(1000 * 120);
         }
-        Thread.sleep(1000 * 5);
       } catch (Exception e) {
         e.printStackTrace();
       }
--- PowerSniffer/src/main/java/com/uf/stock/util/StockUtil.java
+++ PowerSniffer/src/main/java/com/uf/stock/util/StockUtil.java
@@ -1,7 +1,39 @@
 package com.uf.stock.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class StockUtil {
 public static Integer parseStockSymbolToStockCode(String stockSymbol){
   return Integer.parseInt(stockSymbol.substring(2));
+}
+public static boolean isOpenTime(Date date){
+  List<String> openDaysOfWeek=Arrays.asList("Mon","Tue","Wed","Thu","Fri");
+  SimpleDateFormat  formate=new SimpleDateFormat("yyyy-MM-dd");
+  SimpleDateFormat  dayOfWeekFormate=new SimpleDateFormat("E");
+  SimpleDateFormat  hourseFormate=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  try {
+    long nowTime=date.getTime();
+    String day=formate.format(date);
+    String dayOfWeek=dayOfWeekFormate.format(date);
+    if(!openDaysOfWeek.contains(dayOfWeek)){
+      return false;
+    }
+    Date begin1=hourseFormate.parse(day+" 09:30:00");
+    Date end1=hourseFormate.parse(day+" 11:30:00");
+    
+    Date begin2=hourseFormate.parse(day+" 13:00:00");
+    Date end2=hourseFormate.parse(day+" 15:00:00");
+    if((nowTime>=begin1.getTime()&&nowTime<=end1.getTime())||(nowTime>=begin2.getTime()&&nowTime<=end2.getTime())){
+      return true;
+    }
+  } catch (ParseException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
+  return false;
 }
 }
