7f5d1c3250ab3b6af0239426432166f1c13e2799
jason <zzzjjss@gmail.com>
2017-11-17 14:04:33 +0800

    add common action

---
+++ webService/src/main/java/com/uf/store/restful/action/manager/CommonAction.java
@@ -0,0 +1,13 @@
+package com.uf.store.restful.action.manager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping("common")
+@Api(tags = "the api opened for public ")
+public class CommonAction {
+
+}
