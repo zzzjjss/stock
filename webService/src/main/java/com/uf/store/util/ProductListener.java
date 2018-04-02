cc09b55122f479b27cc2a542896ac22f8ff5565a
zhangja <zhangja@ZHANGJA-7W.analytics.moodys.net>
2018-04-02 17:37:26 +0800

    add product  by  "start"  trig file

--- webService/src/main/java/com/uf/store/util/ProductListener.java
+++ webService/src/main/java/com/uf/store/util/ProductListener.java
@@ -25,6 +25,7 @@ import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.io.FileExistsException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -57,25 +58,25 @@ public class ProductListener {
 					Path path = Paths.get(listenFolder);
 					path.register(watcher, StandardWatchEventKinds.ENTRY_CREATE);
 					WatchKey key = null;
-					int i=0;
-					while((key=watcher.take())!=null) {
+					while ((key = watcher.take()) != null) {
 						List<WatchEvent<?>> events = key.pollEvents();
 						for (WatchEvent<?> event : events) {
 							try {
 								WatchEvent.Kind<?> kind = event.kind();
 								if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
 									Path eventPath = (Path) event.context();
-									Path prodPath = path.resolve(eventPath);
-									if (prodPath.toFile().isDirectory()) {
-										logger.info(prodPath.toFile().getAbsolutePath() + ":" + kind.name());
-//										pool.execute(new AddProductTask(prodPath.toFile()));
-										new AddProductTask(prodPath.toFile()).run();
-										i++;
-										logger.info(i+" save product "+prodPath.toFile().getAbsolutePath());
+									File prodPath = path.resolve(eventPath).toFile();
+									if (prodPath.isFile() && prodPath.getName().equals("start")) {
+										File all[] = prodPath.getParentFile().listFiles();
+										for (File p : all) {
+											if (p.isDirectory()) {
+												new AddProductTask(p).run();
+											}
+										}
+										prodPath.delete();
 									}
 								}
-								
-							}catch (Exception e) {
+							} catch (Exception e) {
 								e.printStackTrace();
 							}
 						}
@@ -95,113 +96,77 @@ public class ProductListener {
 		public AddProductTask(File productPath) {
 			folder = productPath;
 		}
+
 		@Override
 		public void run() {
 			if (folder.exists() && folder.isDirectory()) {
-				List<String> prods = new ArrayList<String>();
 				File files[] = null;
-				int i = 10;
-				Product product=new Product();
-				List<ProductProperties> productProperties=new ArrayList<ProductProperties>();
-				List<String> imgs=null;
-				while(true) {
-					files=folder.listFiles();
-					if(files!=null&&files.length>0) {
-						for(File file:files) {
-							try {
-								if(file.getName().equalsIgnoreCase("productInfo.properties")) {
-									Properties properties = new Properties();
-									Path path = file.toPath();
-						            Stream <String> lines = Files.lines(path);
-						            List <String> replaced = lines.map(line -> line.replaceAll("：", ":")).collect(Collectors.toList());
-						            Files.write(path, replaced);
-						            lines.close();
-						            FileInputStream fInputStream=new FileInputStream(file);
-									properties.load(new InputStreamReader(fInputStream, "UTF-8"));	
-									Set<String> keys=properties.stringPropertyNames();
-									for(String key:keys) {
-										String value=properties.getProperty(key);
-										if (key.equalsIgnoreCase("name")) {
-											product.setName(value);
-											continue;
-										}
-										if(key.equalsIgnoreCase("price")) {
-											product.setSellPrice(Float.parseFloat(value));
-											continue;
-										}
-										if(key.equalsIgnoreCase("brand")) {
-											product.setBrand(value);
-											product.setSearchKeywords(value);
-											continue;
-										}
-										if(key.equalsIgnoreCase("imgs")) {
-											imgs=Splitter.on(",").splitToList(value);
-											continue;
-										}
-										if(key.equalsIgnoreCase("id")&&NumberUtils.isCreatable(value)) {
-											Product product2=productService.getProductById(Long.parseLong(value));
-											if (product2!=null) {
-												product.setId(product2.getId());
-												product.setSearchKeywords(product2.getSearchKeywords());
-											}
-											continue;
-										}
-										ProductProperties pro=new ProductProperties();
-										pro.setProduct(product);
-										pro.setPropKey(key);
-										pro.setPropValue(value);
-										productProperties.add(pro);
-									}
-									fInputStream.close();
-									break;
-								}
-							} catch (Exception e) {
-								logger.error("",e);
-							}
-						}
-					}
-					i--;
-					if(imgs!=null||i==0) {
-						break;
-					}else {
+				Product product = new Product();
+				Map<String, byte[]> images = new HashMap<String, byte[]>();
+				List<ProductProperties> productProperties = new ArrayList<ProductProperties>();
+				files = folder.listFiles();
+				if (files != null && files.length > 0) {
+					for (File file : files) {
 						try {
-							logger.info("wait productInfo.properties   ");
-							Thread.sleep(3*1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
+							String fileExtension=FilenameUtils.getExtension(file.getName());
+							if (file.getName().equalsIgnoreCase("productInfo.properties")) {
+								Properties properties = new Properties();
+								Path path = file.toPath();
+								Stream<String> lines = Files.lines(path);
+								List<String> replaced = lines.map(line -> line.replaceAll("：", ":"))
+										.collect(Collectors.toList());
+								Files.write(path, replaced);
+								lines.close();
+								FileInputStream fInputStream = new FileInputStream(file);
+								properties.load(new InputStreamReader(fInputStream, "UTF-8"));
+								Set<String> keys = properties.stringPropertyNames();
+								for (String key : keys) {
+									String value = properties.getProperty(key);
+									if (key.equalsIgnoreCase("name")) {
+										product.setName(value);
+										continue;
+									}
+									if (key.equalsIgnoreCase("price")) {
+										product.setSellPrice(Float.parseFloat(value));
+										continue;
+									}
+									if (key.equalsIgnoreCase("brand")) {
+										product.setBrand(value);
+										product.setSearchKeywords(value);
+										continue;
+									}
+									if (key.equalsIgnoreCase("id") && NumberUtils.isCreatable(value)) {
+										Product product2 = productService.getProductById(Long.parseLong(value));
+										if (product2 != null) {
+											product.setId(product2.getId());
+											product.setSearchKeywords(product2.getSearchKeywords());
+										}
+										continue;
+									}
+									ProductProperties pro = new ProductProperties();
+									pro.setProduct(product);
+									pro.setPropKey(key);
+									pro.setPropValue(value);
+									productProperties.add(pro);
+								}
+								fInputStream.close();
+								break;
+							} else if ("jpg".equalsIgnoreCase(fileExtension)||"png".equalsIgnoreCase(fileExtension)) {
+								images.put(file.getName(), FileUtils.readFileToByteArray(file));
+							}
+						} catch (Exception e) {
+							logger.error("", e);
 						}
 					}
 				}
-				
-				if (imgs != null) {
-					Map<String, byte[]> images = new HashMap<String, byte[]>();
-					waitCopy:while(true) {
-						for (String imgInfo:imgs) {
-							try {
-								List<String> info=Splitter.on("=").splitToList(imgInfo);
-								File imgFile=new File(folder,info.get(0));
-								if (imgFile.length()==Long.parseLong(info.get(1))) {
-									images.put(imgFile.getName(), FileUtils.readFileToByteArray(imgFile));
-								}else {
-									logger.info("wait img copy ");
-									Thread.sleep(1000);
-									continue waitCopy;
-								}
-							} catch (Exception e) {
-								logger.error("", e);
-							}
-						}
-						break;
-					}
-					product.setUpdateTime(new Date());
-					try {
-						productService.saveProduct(product, images);
-						productService.saveProductProperties(productProperties);
-						logger.info("add  product  from " + folder.getAbsolutePath());
-						FileUtils.deleteDirectory(folder);
-					} catch (Exception e) {
-						logger.error("",e);
-					}
+				product.setUpdateTime(new Date());
+				try {
+					productService.saveProduct(product, images);
+					productService.saveProductProperties(productProperties);
+					logger.info("add "+folder.getName()+" to system successful!");
+					FileUtils.deleteDirectory(folder);
+				} catch (Exception e) {
+					logger.error("", e);
 				}
 			}
 		}
